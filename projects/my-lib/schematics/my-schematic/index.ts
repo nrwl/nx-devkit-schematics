f67c10dc34b6d2cdd9e4852e1aec48f8f73dfe4e
Brandon Roberts <brandon@nrwl.io>
2021-05-21 15:13:08 -0500

    add service files

--- projects/my-lib/schematics/my-schematic/index.ts
+++ projects/my-lib/schematics/my-schematic/index.ts
@@ -1,7 +1,26 @@
-import { Tree, convertNxGenerator } from '@nrwl/devkit';
+import { Tree, convertNxGenerator, generateFiles, joinPathFragments, names, readJson } from '@nrwl/devkit';
 
-export function mySchematicGenerator(_tree: Tree, opts: any) {
-  console.log('options', opts);
+export function mySchematicGenerator(tree: Tree, opts: any) {
+  const workspace = readJson(tree, 'angular.json');
+
+  if (!opts.project) {
+    opts.project = workspace.defaultProject;
+  }
+
+  const project = workspace.projects[opts.project];
+  const projectType = project.projectType === 'application' ? 'app' : 'lib';
+
+  if (opts.path === undefined) {
+    opts.path = `${project.sourceRoot}/${projectType}`;
+  }
+
+  const { className, fileName } = names(opts.name);
+
+  generateFiles(tree, joinPathFragments(__dirname, './files'), joinPathFragments(opts.path), {
+    className,
+    name: fileName,
+    tpl: ''
+  });
 }
 
 export const mySchematic = convertNxGenerator(mySchematicGenerator);
